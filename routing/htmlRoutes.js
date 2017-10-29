40fd9d45a4afbc3be2ba4779a1b9a05bec82af13
Sam Sadler <samsadler@sams-mbp.attlocal.net>
2017-10-28 21:15:07 -0500

    server now works

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -11,20 +11,22 @@ var answers = [];
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// App listening
-app.listen(PORT, function(){
-	console.log("App is listening on POT " + PORT);
-});
+
 
 // Routing
-app.get("../public/survey", function(req, res){
+app.get("/public/survey", function(req, res){
 	res.sendFile(path.join(__dirname, '../public/survey.html'))
 });
 
 // Need to store the answers from /public/survery into answers array
-app.post("../public/survey", function (req, res){
+app.post("/public/survey", function (req, res){
 	var friend = req.body;
 	console.log(friend);
 })
 // Match that with the friends from app/data/friends.js
 // Display information in Module
+
+// App listening
+app.listen(PORT, function(){
+	console.log("App is listening on POT " + PORT);
+});
