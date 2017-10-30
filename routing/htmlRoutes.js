ef74cfe50aa1104687de2fae7e2c0122215c5832
Sam Sadler <samsadler@Sams-MacBook-Pro.local>
2017-10-30 18:23:12 -0500

    now returns the friend that matches

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -15,9 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-
 // Routing
-app.get("/public/survey", function(req, res){
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, '../public/home.html'))
+});
+// Routing
+app.get("/survey", function(req, res){
 	res.sendFile(path.join(__dirname, '../public/survey.html'))
 });
 
@@ -25,9 +28,9 @@ app.get("/public/survey", function(req, res){
 app.post("/public/survey", function (req, res){
 	var friend = req.body;
 	answers.push(friend);
-	comparison();
+	var theirFriend = comparison();
 	// console.log(answers);
-	res.end();
+	res.json({ result: theirFriend });
 });
 // Match that with the friends from app/data/friends.js
 // Display information in Module
@@ -38,10 +41,42 @@ app.listen(PORT, function(){
 });
 
 function comparison(){
-	console.log(answers);
+	// console.log(answers);
+	var newAnswer = answers[0];
+	var newAnswerArray = [];
+	var matchDifference = 1000;
+	var matchName = '';
+
+
+	newAnswerArray.push(parseInt(newAnswer.q1));
+	newAnswerArray.push(parseInt(newAnswer.q2));
+	newAnswerArray.push(parseInt(newAnswer.q3));
+	newAnswerArray.push(parseInt(newAnswer.q4));
+	newAnswerArray.push(parseInt(newAnswer.q5));
+	newAnswerArray.push(parseInt(newAnswer.q6));
+	newAnswerArray.push(parseInt(newAnswer.q7));
+	newAnswerArray.push(parseInt(newAnswer.q8));
+	newAnswerArray.push(parseInt(newAnswer.q9));
+	newAnswerArray.push(parseInt(newAnswer.q10));
+
+	console.log(newAnswerArray);
+	console.log('-------------------------');
+
 	for (var i = 0; i < friends.length; i++) {
-		console.log(friends[i].scores);
-	}
+        console.log(friends[i].name + ' ' + friends[i].scores);
+        totalDifference = 0;
+        for (var k = 0; k < 10; k++) {
+            totalDifference += Math.abs(newAnswerArray[k] - friends[i].scores[k]);
+            console.log(totalDifference);
+        }
+        if (totalDifference <= matchDifference) {
+                matchDifference = totalDifference;
+                matchName = friends[i].name;
+
+            }
+
+    }
+    return matchName;
 }
 
 
